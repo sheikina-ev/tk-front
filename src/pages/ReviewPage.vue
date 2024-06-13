a6aabeb326a782b02c4ab6484d522f618bc55e3f
file-hosting.ru <liza.sheykina.2005@mail.ru>
2024-06-13 16:20:10 +0700

    Обновление компонента отзывов

--- src/pages/ReviewPage.vue
+++ src/pages/ReviewPage.vue
@@ -1,21 +1,22 @@
 <template>
   <base-layout page-title="Оставить отзыв">
-    <ion-grid>
-      <ion-row>
-        <ion-col>
-          <form @submit.prevent="submitReview" id="review-form">
-            <p>Оцените, пожалуйста, наш сервис и качество продуктов. Напишите сюда замечания, пожелания, так же мы будем рады предложениям.</p>
-            <p>Можете упоминать сотрудников, их имена есть на бэйджах.</p>
-            <div class="flex-center">
+    <ion-grid class="px-20 lg:px-80">
+      <ion-row class="flex justify-center">
+        <ion-col size-md="8">
+          <form @submit.prevent="submitReview" id="review-form" class="bg-white p-8 rounded mt-40 shadow-md mb-60">
+            <p class="text-lg mb-4">Оцените, пожалуйста, наш сервис и качество продуктов. Напишите сюда замечания, пожелания, так же мы будем рады предложениям.</p>
+            <p class="text-lg mb-4">Можете упоминать сотрудников, их имена есть на бэйджах.</p>
+            <div class="flex justify-center mb-6">
               <star-rating @update:rating="setRating" :show-rating="false" :star-size="24" :padding="20"></star-rating>
               <input type="hidden" v-model="reviewData.rating" />
             </div>
-            <ion-textarea v-model="reviewData.message" name="message" id="message" required placeholder="Текст отзыва*"></ion-textarea>
-            <ion-button type="submit" expand="block">Отправить</ion-button>
+            <ion-textarea v-model="reviewData.message" name="message" id="message" required placeholder="Текст отзыва*" class="w-full mb-6"></ion-textarea>
+            <ion-button type="submit" expand="block" class="w-full ">Отправить</ion-button>
           </form>
         </ion-col>
       </ion-row>
     </ion-grid>
+    <AppFooter/>
   </base-layout>
 </template>
 
@@ -23,9 +24,10 @@
 import { IonGrid, IonRow, IonCol, IonTextarea, IonButton, toastController } from '@ionic/vue';
 import StarRating from 'vue-star-rating';
 import BaseLayout from "@/components/base/BaseLayout.vue";
+import AppFooter from "@/components/base/AppFooter.vue";
 
 export default {
-  components: {BaseLayout, IonGrid, IonRow, IonCol, IonTextarea, IonButton, StarRating },
+  components: {AppFooter, BaseLayout, IonGrid, IonRow, IonCol, IonTextarea, IonButton, StarRating },
   data() {
     return {
       reviewData: {

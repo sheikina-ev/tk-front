d61b708e99f0fd93541a88cc95628cd17044bc4f
DeadPage7 <lolol.kek154@gmail.com>
2025-03-31 15:07:19 +0700

    Улучшенная выборка точек

--- src/components/base/BaseLayout.vue
+++ src/components/base/BaseLayout.vue
@@ -2,65 +2,72 @@
   <ion-page id="main">
     <ion-header>
       <ion-toolbar>
-        <div class="header-container bg-custom-color flex items-center justify-between px-5 md:px-0 relative z-10">
+        <div class="header-container bg-[#f9d9b8] flex items-center justify-between px-5 md:px-0 relative z-10">
           <router-link to="/" class="logo-link">
             <img src="@/assets/img/logoHeader.png" alt="Logo" class="logo mx-4 my-4 w-24 md:ml-315" />
           </router-link>
-          <div class="flex items-center ml-auto relative text-black md:text-black">
-            <div class="coffee-shop-wrap">
-              <div
-                  class="name relative cursor-pointer"
-                  @mouseover="showAddresses = true"
-                  @mouseleave="showAddresses = false"
-              >
-                <span v-if="activeShop">{{ activeShop.store_name }}</span>
-                <span v-else>Точка не выбрана</span>
-                <div
-                    v-if="showAddresses"
-                    class="addresses absolute bg-custom-color1 rounded-md w-48 top-full left-0 shadow-lg "
-                    ref="addresses"
-                >
-                  <ul v-if="activeShop" class="list-none p-0 m-0">
-                    <li
-                        v-for="address in activeShop.addresses"
-                        :key="address.id"
-                        @click="selectAddress(address)"
-                        class="py-2 border border-gray-300 rounded-md cursor-pointer hover:bg-gray-50"
-                    >
-                      {{ address }}
-                    </li>
-                  </ul>
-                  <ul v-else class="list-none p-0 m-0">
-                    <li
-                        v-for="shop in shops"
-                        :key="shop.id"
-                        @click="selectShop(shop)"
-                        class="px-4 py-2 border border-gray-300 rounded-md cursor-pointer hover:bg-gray-300"
-                    >
-                      {{ shop.store_name }}
-                    </li>
-                  </ul>
-                </div>
+
+          <!-- Контейнер для кнопок выбора точки и корзины -->
+          <div class="action-buttons flex items-center space-x-1">
+            <!-- Кнопка для выбора точки -->
+            <ion-button fill="clear" @click="openModal" class="select-shop-button">
+              <ion-card class="shop-card" :class="{ 'selected': activeShop }">
+                <ion-card-content class="text-center text-[#61473b]">
+                  <span v-if="activeShop">{{ activeShop.store_name }}</span>
+                  <span v-else>Выбрать точку</span>
+                </ion-card-content>
+              </ion-card>
+            </ion-button>
+
+            <!-- Кнопка корзины -->
+            <ion-button fill="clear" @click="$router.push('/cart')" class="basket-button p-0">
+              <div class="basket-container relative flex items-center">
+                <ion-card class="basket-card">
+                  <img src="../../../public/assets/img/basket.png" height="28" width="33" alt="Basket Icon" class="basket-icon m-0" />
+                  <ion-badge v-if="cartCount > 0" color="danger" class="badge ml-2 absolute top-0 right-0">
+                    {{ cartCount }}
+                  </ion-badge>
+                </ion-card>
+              </div>
+            </ion-button>
+          </div>
+
+          <!-- Модальное окно -->
+          <ion-modal :is-open="isModalOpen" @didDismiss="closeModal">
+            <div class="modal-container p-4">
+              <div class="modal-content">
+                <h2 class="text-lg font-bold mb-4 text-white text-center">Выберите точку</h2>
+                <ul class="list-none p-0">
+                  <li
+                      v-for="shop in shops"
+                      :key="shop.id"
+                      @click="selectShop(shop)"
+                      class="cursor-pointer py-2 px-4 rounded-md transition-colors text-center"
+                      :class="{ 'bg-[#61473b] text-white': activeShop && activeShop.id === shop.id, 'bg-[#f9d9b8] text-black': !(activeShop && activeShop.id === shop.id) }"
+                  >
+                    {{ shop.store_name }}
+                    <ul v-if="activeShop && activeShop.id === shop.id" class="mt-2">
+                      <li
+                          v-for="address in shop.addresses"
+                          :key="address.id"
+                          @click.stop="selectAddress(shop, address)"
+                          class="py-1 px-3 rounded-md cursor-pointer transition-colors text-center"
+                          :class="{ 'bg-[#61473b] text-white': selectedAddress === address, 'bg-[#f9d9b8] text-black': selectedAddress !== address }"
+                      >
+                        {{ address }}
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+                <ion-button expand="full" @click="closeModal" class="close-button">Закрыть</ion-button>
               </div>
             </div>
-          </div>
-          <ion-button fill="clear" @click="$router.push('/cart')" class="basket-button p-0 md:mr-315">
-            <div class="basket-container relative flex items-center">
-              <img
-                  src="../../../public/assets/img/basket.png"
-                  height="28"
-                  width="33"
-                  alt="Basket Icon"
-                  class="basket-icon m-0"
-              />
-              <ion-badge v-if="cartCount > 0" color="danger" class="badge ml-2 absolute top-0 right-0">
-                {{ cartCount }}
-              </ion-badge>
-            </div>
-          </ion-button>
+          </ion-modal>
+
         </div>
       </ion-toolbar>
     </ion-header>
+
     <ion-content>
       <slot />
     </ion-content>
@@ -68,7 +75,7 @@
 </template>
 
 <script>
-import { IonPage, IonHeader, IonToolbar, IonContent, IonButton, IonBadge } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonContent, IonButton, IonBadge, IonModal, IonCard, IonCardContent } from '@ionic/vue';
 
 export default {
   components: {
@@ -78,10 +85,14 @@ export default {
     IonContent,
     IonButton,
     IonBadge,
+    IonModal,
+    IonCard,
+    IonCardContent,
   },
   data() {
     return {
-      showAddresses: false,
+      isModalOpen: false,
+      selectedAddress: null,
     };
   },
   computed: {
@@ -96,23 +107,85 @@ export default {
     },
   },
   methods: {
-    async selectShop(shop) {
+    openModal() {
+      this.isModalOpen = true;
+    },
+    closeModal() {
+      this.isModalOpen = false;
+      this.selectedAddress = null;
+    },
+    selectShop(shop) {
       this.$store.commit('selectShop', { shopId: shop.id });
-      this.showAddresses = false;
+    },
+    async selectAddress(shop, address) {
+      this.selectedAddress = address;
+      this.$store.commit('setActiveShop', { shopId: shop.id, address });
+      this.isModalOpen = false;
       try {
-        await this.loadProducts(shop.id);
+        await this.$store.dispatch('loadProducts', shop.id);
         this.$router.push('/');
       } catch (error) {
         console.error('Ошибка загрузки товаров:', error);
       }
     },
-    selectAddress(address) {
-      console.log('Выбран адрес:', address);
-      this.showAddresses = false;
-    },
-    loadProducts(shopId) {
-      return this.$store.dispatch('loadProducts', shopId);
-    },
   },
 };
 </script>
+
+<style scoped>
+.modal-container {
+  background-color: #f9d9b8;
+  border-radius: 15px;
+  border: 10px solid #61473b;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.modal-content {
+  background-color: #f9d9b8;
+  padding: 60px;
+  border-radius: 15px;
+  border: 10px solid #61473b;
+  text-align: center;
+  width: 500px;
+}
+
+.close-button {
+  background-color: #61473b !important;
+  color: white;
+  margin-top: 10px;
+}
+
+ul li {
+  transition: background-color 0.3s ease;
+}
+
+ul li:hover {
+  background-color: #61473b;
+  color: white;
+}
+
+.select-shop-button ion-card {
+  background-color: transparent; /* Убираем фон */
+  box-shadow: none; /* Убираем тень */
+  padding-left: 8px;
+  padding-right: 8px;
+}
+
+.basket-button ion-card {
+  background-color: transparent; /* Убираем фон */
+  box-shadow: none; /* Убираем тень */
+  color: #61473b; /* Устанавливаем цвет текста кнопки */
+}
+
+.action-buttons {
+  margin-left: 0px; /* Уменьшаем отступ слева */
+  gap: 8px; /* Уменьшаем отступ между кнопками */
+}
+
+.basket-button {
+  margin-left: -8px; /* Перемещаем кнопку корзины чуть левее */
+}
+</style>
